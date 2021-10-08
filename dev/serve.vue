fe950b1602bdcad688260e9e8804e37f57a5cfa9
Jose Arguello <argelloestigarribia.josantonio@gmail.com>
2021-10-08 13:45:37 -0300

    commit inicial

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueSummernote from '@/vue-summernote.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueSummernote
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-summernote />
+  </div>
+</template>
